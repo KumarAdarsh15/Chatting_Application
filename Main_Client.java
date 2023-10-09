0b2ce07ebfd2084f55bdfe6625de74c63c6e2072
Adarsh Kumar <133121827+KumarAdarsh15@users.noreply.github.com>
2023-10-09 21:42:57 +0530

    Main_Client.java

--- Main_Client.java
+++ Main_Client.java
@@ -2,6 +2,5 @@ public class Main_Client {
     public static void main(String[] args) {
         Client c = new Client();    //invoke GUI part
         c.setIOStreams();           //set's streams through which data will be transferred
-
     }
 }
